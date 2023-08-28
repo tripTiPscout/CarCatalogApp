bdd40df11ad6ee681a6ab4ba00307fe220474418
Peter Stoilkov <peter_sto@mail.bg>
2023-08-28 10:37:21 +0300

    Add Spring to the project

---
+++ src/main/java/com/compactsmartsolutions/carcatalog/CarCatalogAppApplication.java
@@ -0,0 +1,13 @@
+package com.compactsmartsolutions.carcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCatalogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCatalogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compactsmartsolutions/carcatalog/CarCatalogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compactsmartsolutions.carcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCatalogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
